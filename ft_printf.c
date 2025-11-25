1e5ed9fd010725896b57ff067676263531d17e83
Natalia Eremina <nateremi@c2r1s3.42prague.com>
2025-11-25 19:23:11 +0100

    Add .h and main printf

---
+++ ft_printf.c
@@ -0,0 +1,54 @@
+#include "ft_printf.h"
+
+int	handle_function(char specifier, va_list arg)
+{
+	int	count;
+
+	count = 0;
+	if (specifier == '%')
+		count = handle_char('%');
+	else if (specifier == 'c')
+		count = handle_char(va_arg(arg, int));
+	else if (specifier == 's')
+		count = handle_string(va_arg(arg, char *));
+	else if (specifier == 'i')
+		count = handle_integer(va_arg(arg, int));
+	else if (specifier == 'd')
+		count = handle_number(va_arg(arg, long));
+	else if (specifier == 'u')
+		count = handle_decimal(va_arg(arg, unsigned int));
+	else if (specifier == 'p')
+		count = handle_pointer(va_arg(arg, unsigned long));
+	else if (specifier == 'x' || specifier == 'X')
+		count = handle_hex(va_arg(arg, unsigned long), specifier);
+	return (count);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	args;
+	int	i;
+	int	count;
+
+	va_start(args, str);
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%' && checkspecifier(str[i + 1]))
+			count += handle_function(str[i + 1], args);
+		else if (str[i] == '%' && !checkspecifier(str[i + 1]))
+		{
+			write(1, "Especifier ERROR!", 18);
+			return (count);
+		}
+		else
+		{
+			write(1, str[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -1,1 +1,16 @@
-int	ft_printf(const char *, ...);
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int		ft_printf(const char *, ...);
+int		handle_char(char c);
+int		handle_string(char *str);
+int		handle_number(long number);
+int		handle_hex(unsigned long num, char c);
+int		ft_printfp(unsigned long number);
+char	checkspecifier(char	symbol);
+
+#endif
